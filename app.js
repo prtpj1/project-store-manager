a2cc92c1ead14f956976458a73c6969db7645be5
prtpj1 <prtpj1@gmail.com>
2023-11-01 14:47:14 -0300

    :truck: chore: change index.js :arrow_right: transfer app.js code to index.js to test if this work on vercel

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
-const express = require('express');
-const swaggerUi = require('swagger-ui-express');
-const swaggerDocument = require('./swagger/swagger.json');
+// const express = require('express');
+// const swaggerUi = require('swagger-ui-express');
+// const swaggerDocument = require('./swagger/swagger.json');
 
-const app = express();
-app.use(express.json());
+// const app = express();
+// app.use(express.json());
 
-const productsRouter = require('./routes/productsRoutes');
-const salesRouter = require('./routes/salesRoutes');
-const errorMiddleware = require('./middlewares/errorMiddleware');
+// const productsRouter = require('./routes/productsRoutes');
+// const salesRouter = require('./routes/salesRoutes');
+// const errorMiddleware = require('./middlewares/errorMiddleware');
 
-app.get('/', (_request, response) => {
-  response.send();
-});
+// app.get('/', (_request, response) => {
+//   response.send();
+// });
 
-app.use('/products', productsRouter);
-app.use('/sales', salesRouter);
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+// app.use('/products', productsRouter);
+// app.use('/sales', salesRouter);
+// app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-app.use(errorMiddleware);
-module.exports = app;
+// app.use(errorMiddleware);
+// module.exports = app;
--- index.js
+++ index.js
@@ -1,5 +1,27 @@
-const app = require('./app');
+// const app = require('./app');
 require('dotenv').config();
+
+const express = require('express');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger/swagger.json');
+
+const app = express();
+app.use(express.json());
+
+const productsRouter = require('./routes/productsRoutes');
+const salesRouter = require('./routes/salesRoutes');
+const errorMiddleware = require('./middlewares/errorMiddleware');
+
+app.get('/', (_request, response) => {
+  response.send();
+});
+
+app.use('/products', productsRouter);
+app.use('/sales', salesRouter);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+app.use(errorMiddleware);
+module.exports = app;
 
 app.listen(process.env.PORT, () => {
   console.log(`Escutando na porta ${process.env.PORT}`);
